dd812dae7d64c7d32fa0419cf27f90e617294cf4
fabd <fabrice.denis@gmail.com>
2026-06-02 15:02:44 +0200

    Add Prettier config
    - Add .prettierrc.js with project formatting rules
    - Allow .prettierrc.js through .gitignore

---
+++ .prettierrc.js
@@ -0,0 +1,15 @@
+/* globals module */
+module.exports = {
+  printWidth: 85,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: false,
+  trailingComma: "es5",
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: "always",
+  proseWrap: "never",
+  htmlWhitespaceSensitivity: "strict",
+  endOfLine: "lf",
+};
